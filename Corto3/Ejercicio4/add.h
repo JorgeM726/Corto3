d6a80f42c7e84bff6da59d84cbb8040650155b56
JorgeM726 <118854535+JorgeM726@users.noreply.github.com>
2023-03-23 11:25:15 -0600

    Update add.h

--- Corto3/Ejercicio4/add.h
+++ Corto3/Ejercicio4/add.h
@@ -1,7 +1,9 @@
 #ifndef ADD_H
 #define ADD_H
 
-int add (int a, int b);
-
-
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+int divide(int a, int b);
+float pi();
 #endif /*ADD_H*/
