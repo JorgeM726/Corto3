457b733fc9184da530baea2a855550cacdaa149e
JorgeM726 <118854535+JorgeM726@users.noreply.github.com>
2023-03-23 11:24:50 -0600

    Update add.c

--- Corto3/Ejercicio4/add.c
+++ Corto3/Ejercicio4/add.c
@@ -1,7 +1,27 @@
 #include "add.h"
 
-int add (int a, int b)
-
+int add(int a, int b)
 {
-    return a+b;
+    return a + b;
+}
+
+int subtract(int a, int b)
+{
+    return a - b;
+}
+
+int multiply(int a, int b)
+{
+    return a * b;
+}
+
+int divide(int a, int b)
+{
+    return a / b;
+}
+
+float pi()
+{
+    
+    return 3.141592;
 }
